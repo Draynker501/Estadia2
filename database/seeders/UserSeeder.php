dc9d514d408179a64511fc2f83e9718193b52100
Diego Castaneda <cardsm713@gmail.com>
2025-01-27 09:10:22 -0700

    Corrección en el seeder de user para eliminar la relación directa con permisos

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,25 +15,22 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Crear 10 usuarios con rol Subscriptor y permiso Ninguno
+        // Crear 10 usuarios con rol Subscriptor
         $subscriberRole = Role::where('name', 'Subscriptor')->first();
-        $nonePermission = Permission::where('name', 'Ninguno')->first();
 
-        if (!$subscriberRole || !$nonePermission) {
-            $this->command->warn("El rol 'Subscriptor' o el permiso 'Ninguno' no existen en la base de datos.");
+        if (!$subscriberRole) {
+            $this->command->warn("El rol 'Subscriptor' no existen en la base de datos.");
             return;
         }
 
         // 5 usuarios con email_verified_at
-        User::factory(5)->create()->each(function ($user) use ($subscriberRole, $nonePermission) {
+        User::factory(5)->create()->each(function ($user) use ($subscriberRole) {
             $user->roles()->attach($subscriberRole);
-            $user->permissions()->attach($nonePermission);
         });
 
         // 5 usuarios sin email_verified_at
-        User::factory(5)->unverified()->create()->each(function ($user) use ($subscriberRole, $nonePermission) {
+        User::factory(5)->unverified()->create()->each(function ($user) use ($subscriberRole) {
             $user->roles()->attach($subscriberRole);
-            $user->permissions()->attach($nonePermission);
         });
     }
 }
